e177d5e5b277aeba138f8cd6f294ef8f53a06ad9
logesh8220 <logeshthirumurugan@gmail.com>
2022-12-19 12:47:34 +0530

    f

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+
 // Create GET request
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
